6997dced9fab6574c6f25d07be999740d6c43d3c
Alexis Edgar Loza <47640237+Alexiseloza@users.noreply.github.com>
2019-10-25 16:31:19 -0300

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Footer = ({footer}) =>   (
+
+    <footer className = "nav-wrapper footer darken-3 text-center mt-4">
+        <p> Todos los Derechos Reservados &copy; Buscador de Imagenes  2019 - Powered By 
+        <a href="http://alexisloza.net" rel="noopener noreferrer" target="_blank" className="brand-logo center"> AlexisLoza</a>
+        </p>
+        
+    </footer>
+);
+
+
+export default Footer;
